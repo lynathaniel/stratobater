9edb20081db18be9897e3260c53cd2f0d4a6fe75
lynathaniel <nathanielly09@gmail.com>
2026-02-22 15:42:26 -0800

    chore: add custom app and fretboard background colors to Tailwind config

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -7,6 +7,8 @@ export default {
   theme: {
     extend: {
       colors: {
+        'app-bg': '#3d3d3d',
+        'fretboard-bg': '#D2A464',
         'fretboard-root': '#ef4444', // red-500
         'fretboard-triad': '#3b82f6', // blue-500
         'fretboard-scale': '#9ca3af', // gray-400
